3e410a108117027aa10e6838b1bd5b07b2f57db4
ilkoch008 <ilkoch008@gmail.com>
2020-10-29 23:05:19 +0300

    task7

---
+++ task7/cmake-build-debug/CMakeFiles/task7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task7.dir/main.c.obj"
+  "libtask7.dll.a"
+  "task7.exe"
+  "task7.exe.manifest"
+  "task7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/task7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ task7/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <math.h>
+#define T_NUM 4
+#include <windows.h>
+
+int main() {
+    printf("Hello, World!\n");
+    void * xxx;
+    CreateBitmap(400,400,100,100,xxx);
+    return 0;
+}
